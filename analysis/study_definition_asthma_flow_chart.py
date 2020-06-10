c92d52fc8cb11c3279da2f4aa466d09f3c3d126e
Alex Walker <alex.walker@thedatalab.org>
2020-06-10 18:20:40 +0100

    tweaks to asthma logic and incidence

--- analysis/study_definition_asthma_flow_chart.py
+++ analysis/study_definition_asthma_flow_chart.py
@@ -59,19 +59,26 @@ study = StudyDefinition(
     default_expectations={
         "date": {"earliest": "1970-01-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.2,
+        "incidence": 0.05,
     },
     ## STUDY POPULATION (required)
     population=patients.all(),
     has_asthma=patients.with_these_clinical_events(
         asthma_codes,
         between=["2017-02-28", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.5},
     ),
     asthma_ever=patients.with_these_clinical_events(
         asthma_ever_codes,
         on_or_before="2020-02-29",
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.8},
+    ),
+    asthma_sensitivity=patients.satisfying(
+        """
+        has_asthma OR
+        (asthma_ever AND any_asthma_med)
+        """,
+        return_expectations={"incidence": 0.9},
     ),
     any_asthma_med=patients.satisfying(
         """
@@ -108,113 +115,127 @@ study = StudyDefinition(
         "2019-02-28", "2020-02-29", return_expectations={"incidence": 0.9}
     ),
     copd=patients.with_these_clinical_events(
-        copd_codes, on_or_before="2020-02-29", return_expectations={"incidence": 0.2},
+        copd_codes, on_or_before="2020-02-29", return_expectations={"incidence": 0.05},
     ),
     ### OTHER RESPIRATORY
     other_respiratory=patients.with_these_clinical_events(
         other_respiratory_codes,
         on_or_before="2020-02-29",
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     nebules=patients.with_these_medications(
         nebulised_med_codes,
         between=["2019-02-28", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
+    ),
+    lama_no_ics=patients.satisfying(
+        """(lama_single OR laba_lama) AND NOT (
+        high_dose_ics OR
+        high_dose_ics_single_ing OR
+        high_dose_ics_multiple_ingredient OR
+        low_med_dose_ics_single_ingredient OR
+        low_med_dose_ics_multiple_ingredient OR
+        low_med_dose_ics OR
+        ics_single OR
+        laba_ics OR
+        laba_lama_ics
+        )
+        """,
     ),
     #### HIGH DOSE ICS - all preparation
     high_dose_ics=patients.with_these_medications(
         high_dose_ics_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### HIGH DOSE ICS - single ingredient preparations
     high_dose_ics_single_ing=patients.with_these_medications(
         high_dose_ics_single_ingredient_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### HIGH DOSE ICS - multiple ingredient preparation
     high_dose_ics_multiple_ingredient=patients.with_these_medications(
         high_dose_ics_multiple_ingredient_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     ### LOW-MED DOSE ICS - single ingredient preparations
     low_med_dose_ics_single_ingredient=patients.with_these_medications(
         low_medium_ics_single_ingredient_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     ### LOW-MED DOSE ICS - multiple ingredient preparations
     low_med_dose_ics_multiple_ingredient=patients.with_these_medications(
         low_medium_ics_multiple_ingredient_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     ### LOW-MED DOSE ICS - all preparation
     low_med_dose_ics=patients.with_these_medications(
         low_medium_ics_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### ICS SINGLE CONSTITUENT
     ics_single=patients.with_these_medications(
         ics_single_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### ORAL STEROIDS SINGLE CONSTITUENT
     oral_steroids=patients.with_these_medications(
         oral_steroid_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### SABA SINGLE CONSTITUENT
     saba_single=patients.with_these_medications(
         saba_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### SAMA SINGLE CONSTITUENT
     sama_single=patients.with_these_medications(
         sama_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LABA SINGLE CONSTITUENT
     laba_single=patients.with_these_medications(
         single_laba_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LAMA SINGLE CONSTITUENT
     lama_single=patients.with_these_medications(
         single_lama_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LABA + ICS
     laba_ics=patients.with_these_medications(
         laba_ics_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LABA + LAMA
     laba_lama=patients.with_these_medications(
         laba_lama_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LABA + LAMA + ICS
     laba_lama_ics=patients.with_these_medications(
         laba_lama__ics_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### LTRA SINGLE CONSTITUENT
     ltra_single=patients.with_these_medications(
         leukotriene_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
 )
--- analysis/study_definition_copd_flow_chart.py
+++ analysis/study_definition_copd_flow_chart.py
@@ -23,12 +23,12 @@ study = StudyDefinition(
     default_expectations={
         "date": {"earliest": "1970-01-01", "latest": "today"},
         "rate": "uniform",
-        "incidence": 0.2,
+        "incidence": 0.05,
     },
     ## STUDY POPULATION (required)
     population=patients.all(),
     copd=patients.with_these_clinical_events(
-        copd_codes, on_or_before="2020-02-29", return_expectations={"incidence": 0.2},
+        copd_codes, on_or_before="2020-02-29", return_expectations={"incidence": 0.4},
     ),
     age_cat=patients.satisfying(
         "age >=35 AND age <= 110",
@@ -44,30 +44,30 @@ study = StudyDefinition(
     ever_smoked=patients.with_these_clinical_events(
         filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
         on_or_before="2020-02-29",
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.9},
     ),
     has_follow_up=patients.registered_with_one_practice_between(
-        "2019-02-28", "2020-02-29", return_expectations={"incidence": 0.2},
+        "2019-02-28", "2020-02-29", return_expectations={"incidence": 0.9},
     ),
     recent_asthma=patients.with_these_clinical_events(
         asthma_codes,
         between=["2017-02-28", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     other_respiratory=patients.with_these_clinical_events(
         other_respiratory_codes,
         on_or_before="2020-02-29",
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     #### NEBULES
     nebules=patients.with_these_medications(
         nebulised_med_codes,
         between=["2019-02-28", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
     ltra_single=patients.with_these_medications(
         leukotriene_med_codes,
         between=["2019-11-01", "2020-02-29"],
-        return_expectations={"incidence": 0.2},
+        return_expectations={"incidence": 0.05},
     ),
 )
